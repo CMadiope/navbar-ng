6b4d25ebea9ddf8cda46fcd9d510867a1102fdbd
Confidence Madiope <confidencem97@gmail.com>
2023-07-09 12:49:59 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'navbar-ng';
+
+  menuVariable: boolean = false;
+  menu_icon_variable: boolean = false;
+
+  openMenu() {
+    this.menuVariable = !this.menuVariable;
+    this.menu_icon_variable = !this.menu_icon_variable;
+  }
 }
